2c0100cc668848547cbd96412190543cb9ca6a74
Saeeda Choudhury <saeedakc1@gmail.com>
2020-05-21 02:15:34 -0400

    final

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -22,21 +22,10 @@ data.forEach((dataSort) => {
   });
 
 // Level 2
-// instructions: 
-// Using multiple input tags and/or select dropdowns, write JavaScript code so the user can to set 
-// multiple filters and search for UFO sightings using the following criteria based on the table columns:
-
-// date/time
-// city
-// state
-// country
-// shape
-
-
+// there was an attempt
 
 function myFunction() {
-  // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
+  var input, filter, table, tr, td, i, txtValue, tbody;
   input = document.getElementById("datetime");
   filter = input.value.toUpperCase();
   table = document.getElementById("ufo_table");
@@ -44,7 +33,7 @@ function myFunction() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName(tbody);
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
